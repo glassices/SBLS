2876aae38dfa1e91a14cf6a636b38bbbb471cb65
Dieqiao Feng <glassices@gmail.com>
2021-08-25 16:46:14 -0400

    faster/optimized/better

--- srcs/n6/dataset.py
+++ srcs/n6/dataset.py
@@ -12,70 +12,72 @@ class MyDataset(Dataset):
         self.arrs = []
         for case in cases:
             m = [[int(e) for e in row.split()] for row in case.split('\n')]
-            arr = numpy.array(m, dtype = numpy.int32)
+            n = len(m[0])
+            arr = numpy.empty((n, n), dtype = numpy.int32)
+            arr[0] = range(n)
+            arr[1:] = numpy.array(m, dtype = numpy.int32)
             self.arrs.append(arr)
-            if arr.shape[1] not in self.dt:
-                self.dt[arr.shape[1]] = []
-            self.dt[arr.shape[1]].append(arr)
+            if n not in self.dt:
+                self.dt[n] = []
+            self.dt[n].append(arr)
 
     def __len__(self):
         return 1000000000000000000
 
     def __getitem__(self, index):
-        arr = random.choice(self.arrs)
-        n = arr.shape[1]
-        a = numpy.zeros((n, n), dtype = numpy.int32)
-        a[0] = range(n)
-        a[1:] = arr
+        a = random.choice(self.arrs)
+        n = a.shape[0]
         indices = random.sample([(i, j) for i in range(n) for j in range(n)], random.randint(1, n * n))
+        indices.sort()
+        removed = numpy.zeros((n, n), dtype = bool)
         for index in indices:
-            a[index] = -1
+            removed[index] = True
+        valid_a = a[~removed]
         
         data = torch.zeros(2, n, n, n).float()
         for i in range(n):
             for j in range(n):
-                if a[i, j] != -1:
+                if not removed[i, j]:
                     data[0, a[i, j], i, j] = 1.0
-        data[1] = 1
-        label = torch.zeros(n, n, n).float()
+        data[1] = 1.0
+        label = numpy.zeros((n, n, n), dtype = numpy.float32)
         for par in self.dt[n]:
-            perm = dict()
+            perm = [None] * n
+            revs = [None] * n
             ok = True
-            for i in range(n):
-                for j in range(n):
-                    if a[i, j] == -1:
-                        continue
-                    u = j if i == 0 else par[i - 1, j]
-                    if u not in perm:
-                        perm[u] = a[i, j]
-                    else:
-                        if perm[u] != a[i, j]:
-                            ok = False
-                            break
-                if not ok:
+            valid_par = par[~removed]
+            empty_par = par[removed]
+            for u, v in zip(valid_a, valid_par):
+                if perm[v] is None:
+                    if revs[u] is not None:
+                        ok = False
+                        break
+                    perm[v] = u
+                    revs[u] = v
+                elif perm[v] != u:
+                    ok = False
                     break
 
-            if not ok or len(set(perm.values())) != len(perm):
+            if not ok:
                 continue
-            free = [i for i in range(n) if i not in perm]
-            for i in range(n):
-                for j in range(n):
-                    if a[i, j] != -1:
-                        continue
-                    u = j if i == 0 else par[i - 1, j]
-                    if u not in perm:
-                        label[free, i, j] = 1.0
-                    else:
-                        label[perm[u], i, j] = 1.0
 
-        return data, label
+            free = [i for i in range(n) if perm[i] is None]
+            for v, (x, y) in zip(empty_par, indices):
+                if perm[v] is None:
+                    label[free, x, y] = 1.0
+                else:
+                    label[perm[v], x, y] = 1.0
+
+        return data, torch.from_numpy(label)
 
 if __name__ == '__main__':
+    random.seed(58)
     dataset = MyDataset()
     dataloader = DataLoader(dataset, batch_size = 10)
-    for i in range(200):
+    for i in range(50):
         data, label = dataset[i]
         u, v = round(data[0].sum().item()), round(label.sum().item())
         if u + v != 36:
-            print(u, v)
+            print(data.sum(), u, v)
+    print(dataset[0])
 
